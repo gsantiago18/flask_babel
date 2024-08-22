990f1521dfb93c70be3e37e36604a41898d46033
Gabriel Santiago Gonzalez Leyton <analista.sistemas@plafilm.com>
2024-08-22 09:56:57 -0500

    conectando front

--- babel/app.py
+++ babel/app.py
@@ -1,32 +1,77 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,g
 from flask_babel import Babel
 from flask_babel import gettext
 
-app = Flask(__name__)
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en' # es, fr
-app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
 
-babel = Babel(app)
+class user:
+    def __init__(self,locale='es') -> None:
+        self.locale = locale
 
-@babel.localeselector
+
+
 def get_locale():
-    lang= request.json('lang')
-    return lang or app.config
+        
+    user = getattr(g, 'user', None)
+    if user is not None:
+        return user.locale
+    locale= request.headers.get('Accept-Language')
+    if locale:
+        return locale.split(',')[0][:2]
+    
+    return request.accept_languages.best_match(['es','en','fr'])
+    
 
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'es' # es, fr
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'     
 
+babel = Babel(app,locale_selector=get_locale)
+
+    
+    
+    
 
 @app.route('/')
 def index():
     print('LLEGO POR AQUI')
+    data=request.get_json()
+    locale= data.get('locale')
+    if locale:
+        g.user = user(locale)
     return jsonify({'message': gettext("Hello world!!")})
 
-@app.route('/save')
+@app.route('/save', methods=['GET'])
 def save():
+    data=request.get_json()
+    locale= data.get('locale')
+    if locale:
+        g.user = user(locale)
+    
     return jsonify(message=gettext("Save sucesfuly"))
 
 @app.route('/update')
 def update():
+    data=request.get_json()
+    locale= data.get('locale')
+    if locale:
+        g.user = user(locale)
+    return jsonify(message=gettext("Update changes"))
+
+@app.route('/delete')
+def delete():
+    data=request.get_json()
+    locale= data.get('locale')
+    if locale:
+        g.user = user(locale)
+    return jsonify(message=gettext("Delete  sucesfully"))
+
+@app.route('/change')
+def change():
+    data=request.get_json()
+    locale= data.get('locale')
+    if locale:
+        g.user = user(locale)
     return jsonify(message=gettext("Saves changes"))
 
 
